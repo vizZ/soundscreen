3fe209aca244186a51cb1531c7aa256d53274b56
Artur Glier <arturglier@gmail.com>
2013-11-11 20:43:57 +0100

    Added DataContract interface

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/DataContract.java
@@ -0,0 +1,6 @@
+package com.arturglier.mobile.android.soundscreen.data;
+
+public interface DataContract {
+    String COMMON_CONTENT_TYPE = "vnd.arturglier.mobile.android.soundscreen.cursor.dir";
+    String COMMON_CONTENT_ITEM_TYPE = "vnd.arturglier.mobile.android.soundscreen.cursor.item";
+}

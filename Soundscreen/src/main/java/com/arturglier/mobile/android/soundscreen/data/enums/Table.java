20688f179688822b0f2b1ae85b1c6d73c164b3f2
Artur Glier <arturglier@gmail.com>
2013-11-11 22:31:06 +0100

    Table enum constructor now needs both DataContract and DataMatcher as params

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/enums/Table.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/enums/Table.java
@@ -3,14 +3,19 @@ package com.arturglier.mobile.android.soundscreen.data.enums;
 import com.arturglier.mobile.android.soundscreen.data.DataContract;
 import com.arturglier.mobile.android.soundscreen.data.contracts.TracksContract;
 import com.arturglier.mobile.android.soundscreen.data.contracts.UsersContract;
+import com.arturglier.mobile.android.soundscreen.data.matchers.DataMatcher;
+import com.arturglier.mobile.android.soundscreen.data.matchers.TracksMatcher;
+import com.arturglier.mobile.android.soundscreen.data.matchers.UsersMatcher;
 
 public enum Table {
-    USERS(UsersContract.class),
-    TRACKS(TracksContract.class);
+    USERS(UsersContract.class, UsersMatcher.class),
+    TRACKS(TracksContract.class, TracksMatcher.class);
 
-    private final DataContract contract;
+    public final DataContract contract;
+    public final DataMatcher matcher;
 
-    Table(Class<? extends DataContract> clazz) throws IllegalAccessException, InstantiationException {
-        this.contract = clazz.newInstance();
+    Table(Class<? extends DataContract> contractClass, Class<? extends DataMatcher> matcherClass) throws IllegalAccessException, InstantiationException {
+        this.contract = contractClass.newInstance();
+        this.matcher = matcherClass.newInstance();
     }
 }

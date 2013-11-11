ee560c1629633d51efd3e6a938d35b599551cb6a
Artur Glier <arturglier@gmail.com>
2013-11-11 20:49:03 +0100

    Added UsersColumns interface

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/UsersColumns.java
@@ -0,0 +1,6 @@
+package com.arturglier.mobile.android.soundscreen.data.contracts;
+
+public interface UsersColumns extends CommonColumns {
+    String USERNAME = "username";
+    String AVATAR_URL = "avatar_url";
+}

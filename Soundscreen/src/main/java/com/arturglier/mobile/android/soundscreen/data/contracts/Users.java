9aae228bbcc965c0e8e79458bf28e24a954f0c67
Artur Glier <arturglier@gmail.com>
2013-11-11 20:54:50 +0100

    Added Users contract class

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/Users.java
@@ -0,0 +1,15 @@
+package com.arturglier.mobile.android.soundscreen.data.contracts;
+
+import android.net.Uri;
+
+import com.arturglier.mobile.android.soundscreen.data.DataContract;
+
+public class Users implements DataContract, UsersColumns {
+    public static final String MODEL_NAME = "user";
+    public static final String TABLE_NAME = "users";
+
+    public static final Uri CONTENT_URI = COMMON_CONTENT_URI.buildUpon().appendPath(TABLE_NAME).build();
+
+    public static final String CONTENT_TYPE = COMMON_CONTENT_TYPE + "/" + MODEL_NAME;
+    public static final String CONTENT_ITEM_TYPE = COMMON_CONTENT_ITEM_TYPE + "/" + MODEL_NAME;
+}

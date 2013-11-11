e0421da45afd82f78798ee143f45c6985010b229
Artur Glier <arturglier@gmail.com>
2013-11-11 23:32:15 +0100

    Used SQLBuilder for implementing CREATE_TABLE sql statement for users table

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/UsersHelper.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/UsersHelper.java
@@ -2,10 +2,21 @@ package com.arturglier.mobile.android.soundscreen.data.helpers;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.arturglier.mobile.android.soundscreen.data.contracts.UsersContract;
+
 public class UsersHelper implements DataHelper {
+    private static final String CREATE_TABLE_USERS =
+        SQL.table(UsersContract.TABLE_NAME)
+            .column(UsersContract._ID).asIntegerPrimaryKeyAutoincrement()
+            .column(UsersContract.URI).asText()
+            .column(UsersContract.PERMA_LINK).asText()
+            .column(UsersContract.USERNAME).asText()
+            .column(UsersContract.AVATAR_URL).asText()
+            .create();
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE_USERS);
     }
 
     @Override

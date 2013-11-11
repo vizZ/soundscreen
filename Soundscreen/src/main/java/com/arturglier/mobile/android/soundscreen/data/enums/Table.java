a68c4ff57edc6e999c3da93eebad129cb75cbeb9
Artur Glier <arturglier@gmail.com>
2013-11-11 23:13:02 +0100

    Table enum constructor now needs extra DataHelper param

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/enums/Table.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/enums/Table.java
@@ -3,19 +3,24 @@ package com.arturglier.mobile.android.soundscreen.data.enums;
 import com.arturglier.mobile.android.soundscreen.data.contracts.DataContract;
 import com.arturglier.mobile.android.soundscreen.data.contracts.TracksContract;
 import com.arturglier.mobile.android.soundscreen.data.contracts.UsersContract;
+import com.arturglier.mobile.android.soundscreen.data.helpers.DataHelper;
+import com.arturglier.mobile.android.soundscreen.data.helpers.TracksHelper;
+import com.arturglier.mobile.android.soundscreen.data.helpers.UsersHelper;
 import com.arturglier.mobile.android.soundscreen.data.matchers.DataMatcher;
 import com.arturglier.mobile.android.soundscreen.data.matchers.TracksMatcher;
 import com.arturglier.mobile.android.soundscreen.data.matchers.UsersMatcher;
 
 public enum Table {
-    USERS(UsersContract.class, UsersMatcher.class),
-    TRACKS(TracksContract.class, TracksMatcher.class);
+    USERS(UsersContract.class, UsersMatcher.class, UsersHelper.class),
+    TRACKS(TracksContract.class, TracksMatcher.class, TracksHelper.class);
 
     public final DataContract contract;
     public final DataMatcher matcher;
+    public final DataHelper helper;
 
-    Table(Class<? extends DataContract> contractClass, Class<? extends DataMatcher> matcherClass) throws IllegalAccessException, InstantiationException {
+    Table(Class<? extends DataContract> contractClass, Class<? extends DataMatcher> matcherClass, Class<? extends DataHelper> helperClass) throws IllegalAccessException, InstantiationException {
         this.contract = contractClass.newInstance();
         this.matcher = matcherClass.newInstance();
+        this.helper = helperClass.newInstance();
     }
 }

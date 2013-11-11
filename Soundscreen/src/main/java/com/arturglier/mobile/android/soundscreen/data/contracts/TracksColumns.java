c4c4518fafcbbbc40709b54c3d695647bba87d3b
Artur Glier <arturglier@gmail.com>
2013-11-11 20:49:30 +0100

    Added TracksColumns interface

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/TracksColumns.java
@@ -0,0 +1,15 @@
+package com.arturglier.mobile.android.soundscreen.data.contracts;
+
+public interface TracksColumns extends CommonColumns {
+    String TITLE = "title";
+    String DURATION = "duration";
+    String GENRE = "genre";
+    String DESCRIPTION = "description";
+
+    String WAVEFORM_URL = "waveform_url";
+
+    String PLAYBACK_COUNT = "playback_count";
+    String DOWNLOAD_COUNT = "download_count";
+    String FAVORITINGS_COUNT = "favoritings_count";
+    String COMMENT_COUNT = "comment_count";
+}

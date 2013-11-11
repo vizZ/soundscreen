10e2f6ecdaa31e474f48517a6acd0d4e991e8b19
Artur Glier <arturglier@gmail.com>
2013-11-11 20:53:52 +0100

    squash with created datacontract

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/DataContract.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/DataContract.java
@@ -1,6 +1,10 @@
 package com.arturglier.mobile.android.soundscreen.data;
 
+import android.net.Uri;
+
 public interface DataContract {
+    Uri COMMON_CONTENT_URI = DataProvider.CONTENT_URI;
+    
     String COMMON_CONTENT_TYPE = "vnd.arturglier.mobile.android.soundscreen.cursor.dir";
     String COMMON_CONTENT_ITEM_TYPE = "vnd.arturglier.mobile.android.soundscreen.cursor.item";
 }

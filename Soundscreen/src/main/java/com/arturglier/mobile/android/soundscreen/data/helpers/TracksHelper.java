260fc1edfd6a0d92ff48dee395a121fd39451291
Artur Glier <arturglier@gmail.com>
2013-11-11 23:07:56 +0100

    Added TrackHelper class for tracks table creating and migrations management

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/TracksHelper.java
@@ -0,0 +1,15 @@
+package com.arturglier.mobile.android.soundscreen.data.helpers;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class TracksHelper implements DataHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        throw new UnsupportedOperationException(this.getClass().getSimpleName() + " does not support this operation");
+    }
+}

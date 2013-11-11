6cc8c79a48bf5d007f0440f103bcca72e0c1ed80
Artur Glier <arturglier@gmail.com>
2013-11-11 23:32:27 +0100

    Used SQLBuilder for implementing CREATE_TABLE sql statement for tracks table

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/TracksHelper.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/TracksHelper.java
@@ -2,10 +2,30 @@ package com.arturglier.mobile.android.soundscreen.data.helpers;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.arturglier.mobile.android.soundscreen.data.contracts.TracksContract;
+
 public class TracksHelper implements DataHelper {
+
+    private static final String CREATE_TABLE_TRACKS =
+        SQL.table(TracksContract.TABLE_NAME)
+            .column(TracksContract._ID).asIntegerPrimaryKeyAutoincrement()
+            .column(TracksContract.URI).asText()
+            .column(TracksContract.PERMA_LINK).asText()
+            .column(TracksContract.TITLE).asText()
+            .column(TracksContract.DURATION).asInteger()
+            .column(TracksContract.GENRE).asText()
+            .column(TracksContract.DESCRIPTION).asText()
+            .column(TracksContract.WAVEFORM_URL).asText()
+            .column(TracksContract.PLAYBACK_COUNT).asInteger()
+            .column(TracksContract.DOWNLOAD_COUNT).asInteger()
+            .column(TracksContract.FAVORITINGS_COUNT).asInteger()
+            .column(TracksContract.COMMENT_COUNT).asInteger()
+            .create();
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE_TRACKS);
     }
 
     @Override

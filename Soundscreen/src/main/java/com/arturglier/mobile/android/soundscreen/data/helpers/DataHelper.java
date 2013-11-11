8adf771c95306646a2c6dc1e8ecfd67ca1f793a5
Artur Glier <arturglier@gmail.com>
2013-11-11 23:28:06 +0100

    Added SQLBuilder to DataHelper interface as a static field

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/DataHelper.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/helpers/DataHelper.java
@@ -2,8 +2,12 @@ package com.arturglier.mobile.android.soundscreen.data.helpers;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.arturglier.mobile.android.soundscreen.data.utils.sql.SQLBuilder;
+
 public interface DataHelper {
     public void onCreate(SQLiteDatabase db);
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion);
+
+    SQLBuilder SQL = new SQLBuilder();
 }

fe3b206bcdcbfe7229fff2fb0b76217f9a14ab69
Artur Glier <arturglier@gmail.com>
2013-11-11 23:11:36 +0100

    squash with dataurimatcher

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/DataUriMatcher.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/DataUriMatcher.java
@@ -1,13 +1,12 @@
 package com.arturglier.mobile.android.soundscreen.data;
 
 import com.arturglier.mobile.android.soundscreen.common.utils.CommonUriMatcher;
-import com.arturglier.mobile.android.soundscreen.data.DataProvider;
 import com.arturglier.mobile.android.soundscreen.data.enums.Table;
 
 public class DataUriMatcher extends CommonUriMatcher {
 
     public DataUriMatcher() {
-        super(DataProvider.AUTHORITY);
+        super(DataContentProvider.AUTHORITY);
 
         for (Table table : Table.values()) {
             table.matcher.addURIsTo(this);

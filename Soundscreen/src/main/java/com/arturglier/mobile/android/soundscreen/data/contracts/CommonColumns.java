c1b141b98b1d19775c19520c5e6ab380feb023de
Artur Glier <arturglier@gmail.com>
2013-11-11 20:44:56 +0100

    Added CommonColumns interface

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/CommonColumns.java
@@ -0,0 +1,9 @@
+package com.arturglier.mobile.android.soundscreen.data.contracts;
+
+import android.provider.BaseColumns;
+
+public interface CommonColumns extends BaseColumns {
+    String ID = "id";
+    String URI = "uri";
+    String PERMA_LINK = "permalink_url";
+}

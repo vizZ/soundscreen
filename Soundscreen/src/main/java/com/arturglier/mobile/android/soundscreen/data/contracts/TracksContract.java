1cda07dc731501bca01f32902cddd95413a34f27
Artur Glier <arturglier@gmail.com>
2013-11-16 12:11:25 +0100

    Added new TracksContract.buildWaveformUri(id) helper function

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/TracksContract.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/TracksContract.java
@@ -2,6 +2,8 @@ package com.arturglier.mobile.android.soundscreen.data.contracts;
 
 import android.net.Uri;
 
+import com.arturglier.mobile.android.soundscreen.data.matchers.TracksMatcher;
+
 public class TracksContract implements DataContract, TracksColumns {
     public static final String MODEL_NAME = "track";
     public static final String TABLE_NAME = "tracks";
@@ -10,4 +12,12 @@ public class TracksContract implements DataContract, TracksColumns {
 
     public static final String CONTENT_TYPE = COMMON_CONTENT_TYPE + "/" + MODEL_NAME;
     public static final String CONTENT_ITEM_TYPE = COMMON_CONTENT_ITEM_TYPE + "/" + MODEL_NAME;
+
+    public static Uri buildWaveformUri(Long id) {
+        if (id != null) {
+            return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).appendPath(TracksMatcher.PATH_WAVEFORM).build();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }

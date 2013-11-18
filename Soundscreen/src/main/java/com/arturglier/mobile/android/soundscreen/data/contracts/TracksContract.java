ee12a0cf563c5ed93242349734bc60a2c65310a1
Artur Glier <arturglier@gmail.com>
2013-11-18 21:55:26 +0100

    Added new uri builder functions to TracksContract

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/TracksContract.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/contracts/TracksContract.java
@@ -3,6 +3,7 @@ package com.arturglier.mobile.android.soundscreen.data.contracts;
 import android.net.Uri;
 
 import com.arturglier.mobile.android.soundscreen.data.matchers.TracksMatcher;
+import com.arturglier.mobile.android.soundscreen.data.utils.sql.SQLBuilder;
 
 public class TracksContract implements DataContract, TracksColumns {
     public static final String MODEL_NAME = "track";
@@ -13,11 +14,35 @@ public class TracksContract implements DataContract, TracksColumns {
     public static final String CONTENT_TYPE = COMMON_CONTENT_TYPE + "/" + MODEL_NAME;
     public static final String CONTENT_ITEM_TYPE = COMMON_CONTENT_ITEM_TYPE + "/" + MODEL_NAME;
 
-    public static Uri buildWaveformUri(Long id) {
-        if (id != null) {
-            return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).appendPath(TracksMatcher.PATH_WAVEFORM).build();
-        } else {
+    public static Uri buildUri(Long id) {
+        if (id == null) {
             throw new IllegalArgumentException();
+        } else {
+            return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).build();
         }
+    }
+
+    public static Uri buildWaveformUri(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException();
+        } else {
+            return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).appendPath(TracksMatcher.PATH_WAVEFORMS).build();
+        }
+    }
+
+    public static Uri buildWaveformUri(Uri uri) {
+        if (uri == null) {
+            throw new IllegalArgumentException();
+        } else {
+            return uri.buildUpon().appendPath(TracksMatcher.PATH_WAVEFORMS).build();
+        }
+    }
+
+    public static Uri cached() {
+        return CONTENT_URI.buildUpon().appendQueryParameter(TracksContract.CACHED, SQLBuilder.TRUE).build();
+    }
+
+    public static Uri scheduled() {
+        return TracksContract.cached().buildUpon().appendQueryParameter(TracksContract.USED, SQLBuilder.FALSE).build();
     }
 }

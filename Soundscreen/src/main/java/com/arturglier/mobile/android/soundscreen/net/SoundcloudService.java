2de9de9e1fc1c0a598691f76eff94cd016d38b2e
Artur Glier <arturglier@gmail.com>
2013-11-18 22:32:30 +0100

    Split fetching json data and image data with SoundcloudService

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/net/SoundcloudService.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/net/SoundcloudService.java
@@ -4,9 +4,12 @@ import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.ContentProviderOperation;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.OperationApplicationException;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -16,6 +19,7 @@ import com.arturglier.mobile.android.soundscreen.data.contracts.TracksContract;
 import com.arturglier.mobile.android.soundscreen.data.contracts.UsersContract;
 import com.arturglier.mobile.android.soundscreen.data.models.Track;
 import com.arturglier.mobile.android.soundscreen.data.models.User;
+import com.arturglier.mobile.android.soundscreen.data.utils.sql.SQLBuilder;
 import com.arturglier.mobile.android.soundscreen.net.receivers.ScheduledUpdateReceiver;
 import com.google.gson.Gson;
 import com.soundcloud.api.ApiWrapper;
@@ -23,8 +27,12 @@ import com.soundcloud.api.Http;
 import com.soundcloud.api.Request;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -99,51 +107,105 @@ public class SoundcloudService extends IntentService {
     }
 
     private void handleFetchWaveforms() {
+        getContentResolver().delete(TracksContract.buildWaveformUri(TracksContract.CONTENT_URI), null, null);
+        Cursor query = null;
+        try {
+            query = getContentResolver().query(TracksContract.CONTENT_URI, null, null, null, "RANDOM() LIMIT 10");
+            if (query.moveToFirst()) {
+                do {
+                    Long _id = query.getLong(query.getColumnIndexOrThrow(TracksContract._ID));
+                    String url = query.getString(query.getColumnIndexOrThrow(TracksContract.WAVEFORM_URL));
 
+                    Uri trackUri = TracksContract.buildUri(_id);
+                    fetchAndStoreWaveform(trackUri, url);
+                } while (query.moveToNext());
+            } else {
+                SoundcloudService.fetchFavorites(this);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (query != null) {
+                query.close();
+            }
+        }
     }
 
     private void handleFetchFavorites() {
         HttpResponse resp = null;
         try {
             resp = sWrapper.get(Request.to("/users/mr-scruff/tracks.json"));
-            String content = Http.getString(resp);
-            Log.d("RESPONSE", Http.formatJSON(content));
+            if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String content = Http.getString(resp);
+                Log.d("RESPONSE", Http.formatJSON(content));
 
-            Track[] tracks = sGson.fromJson(content, Track[].class);
-            Log.d("GSON", "Number of Tracks: " + tracks.length);
+                Track[] tracks = sGson.fromJson(content, Track[].class);
+                Log.d("GSON", "Number of Tracks: " + tracks.length);
 
-            ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+                ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
 
-            ops.add(ContentProviderOperation.newDelete(UsersContract.CONTENT_URI).build());
-            ops.add(ContentProviderOperation.newDelete(TracksContract.CONTENT_URI).build());
+                ops.add(ContentProviderOperation.newDelete(UsersContract.CONTENT_URI).build());
+                ops.add(ContentProviderOperation.newDelete(TracksContract.CONTENT_URI).build());
 
-            for (Track track : tracks) {
-                User user = track.getUser();
+                for (Track track : tracks) {
+                    User user = track.getUser();
 
-                ops.add(
-                    ContentProviderOperation
-                        .newInsert(UsersContract.CONTENT_URI)
-                        .withValues(user.toContentValues())
-                        .build()
-                );
+                    ops.add(
+                        ContentProviderOperation
+                            .newInsert(UsersContract.CONTENT_URI)
+                            .withValues(user.toContentValues())
+                            .build()
+                    );
 
-                ops.add(
-                    ContentProviderOperation
-                        .newInsert(TracksContract.CONTENT_URI)
-                        .withValues(track.toContentValues())
-                        .build()
-                );
-            }
+                    ops.add(
+                        ContentProviderOperation
+                            .newInsert(TracksContract.CONTENT_URI)
+                            .withValues(track.toContentValues())
+                            .build()
+                    );
+                }
 
-            try {
-                getContentResolver().applyBatch(DataContentProvider.AUTHORITY, ops);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            } catch (OperationApplicationException e) {
-                e.printStackTrace();
+                try {
+                    getContentResolver().applyBatch(DataContentProvider.AUTHORITY, ops);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                } catch (OperationApplicationException e) {
+                    e.printStackTrace();
+                }
+
+                SoundcloudService.fetchWaveforms(this);
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void fetchAndStoreWaveform(Uri uri, String url) throws IOException {
+        HttpResponse response = sWrapper.getHttpClient().execute(new HttpGet(url));
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            byte[] fileData = EntityUtils.toByteArray(response.getEntity());
+            Log.d("FILE", "File size in bytes: " + fileData.length);
+
+            // Write data to file
+            OutputStream fos = null;
+            try {
+                fos = getContentResolver().openOutputStream(TracksContract.buildWaveformUri(uri));
+                fos.write(fileData);
+                fos.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+                // TODO: add some better handling?
+            } finally {
+                if (fos != null) {
+                    fos.close();
+                }
+            }
+
+            ContentValues values = new ContentValues();
+            values.put(TracksContract.CACHED, SQLBuilder.TRUE);
+            getContentResolver().update(uri, values, null, null);
+
+            Log.d("CACHED", "Cached: " + uri);
         }
     }
 }

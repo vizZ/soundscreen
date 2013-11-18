98ad3a081fa2a54a1541115beebc03c93135100e
Artur Glier <arturglier@gmail.com>
2013-11-18 20:21:41 +0100

    Added SQBuilder's TRUE and FALSE constants

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/utils/sql/SQLBuilder.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/data/utils/sql/SQLBuilder.java
@@ -2,6 +2,10 @@ package com.arturglier.mobile.android.soundscreen.data.utils.sql;
 
 public class SQLBuilder {
 
+    public static String TRUE = "TRUE";
+    public static String FALSE = "FALSE";
+
+
     public SQLTable table(String name) {
         return new SQLTable(name);
     }

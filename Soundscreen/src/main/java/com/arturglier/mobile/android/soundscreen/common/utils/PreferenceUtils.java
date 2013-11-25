bbca8478bdddea316c0d8ddcd918e9b4784791c5
Artur Glier <arturglier@gmail.com>
2013-11-25 12:30:11 +0100

    Added PrerefenceUtils class for easy checking user settings; Added downloadPossible(Context)

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/common/utils/PreferenceUtils.java
@@ -0,0 +1,42 @@
+package com.arturglier.mobile.android.soundscreen.common.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+
+import com.arturglier.mobile.android.soundscreen.R;
+
+public class PreferenceUtils {
+
+    public static boolean downloadPossible(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Integer downloadConf = Integer.valueOf(prefs.getString(context.getString(R.string.pref_download), context.getString(R.string.download_entry_mobile_and_wifi)));
+
+        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        int type = networkInfo.getType();
+        boolean connected = networkInfo.isConnected();
+
+        if (connected) {
+            switch (downloadConf) {
+                case 0:
+                    return false;
+                case 1:
+                    return true;
+                case 2:
+                    if (type == ConnectivityManager.TYPE_MOBILE) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                default:
+                    throw new IllegalArgumentException("Unknown download setting code: " + downloadConf);
+            }
+        } else {
+            return false;
+        }
+    }
+}

6b2b38ede208b59a8b6541793518ca10835a99b1
Artur Glier <arturglier@gmail.com>
2013-11-25 11:55:20 +0100

    Added support for enabling or restricting download over wifi or mobile network

--- Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/SoundscreenPreferenceActivity.java
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/SoundscreenPreferenceActivity.java
@@ -16,16 +16,30 @@ public class SoundscreenPreferenceActivity extends SherlockPreferenceActivity {
         addPreferencesFromResource(R.xml.prefs);
         PreferenceManager.setDefaultValues(this, R.xml.prefs, false);
 
-        final ListPreference listPreference = (ListPreference) findPreference("pref_duration");
-        listPreference.setSummary(getString(R.string.pref_duration_summary, getString(R.string.duration_entry_15sec)));
-        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        final ListPreference duration = (ListPreference) findPreference("pref_duration");
+        duration.setSummary(getString(R.string.pref_duration_summary, getString(R.string.duration_entry_15sec)));
+        duration.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 // Set the value as the new value
-                listPreference.setValue(newValue.toString());
+                duration.setValue(newValue.toString());
                 // Get the entry which corresponds to the current value and set as summary
-                preference.setSummary(getString(R.string.pref_duration_summary, listPreference.getEntry().toString()));
+                preference.setSummary(getString(R.string.pref_duration_summary, duration.getEntry().toString()));
+                return false;
+            }
+        });
+
+        final ListPreference download = (ListPreference) findPreference("pref_download");
+        download.setSummary(getString(R.string.pref_download_summary, getString(R.string.pref_download_entry_mobile_and_wifi)));
+        download.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                // Set the value as the new value
+                download.setValue(newValue.toString());
+                // Get the entry which corresponds to the current value and set as summary
+                preference.setSummary(getString(R.string.pref_download_summary, download.getEntry().toString()));
                 return false;
             }
         });

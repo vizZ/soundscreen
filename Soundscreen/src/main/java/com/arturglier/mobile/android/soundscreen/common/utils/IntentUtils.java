af446fef74b706828d82f5dfc08813e34a89792c
Artur Glier <arturglier@gmail.com>
2013-11-25 02:12:23 +0100

    Added IntentUtils for querying packagemanager for specific intent support

---
+++ Soundscreen/src/main/java/com/arturglier/mobile/android/soundscreen/common/utils/IntentUtils.java
@@ -0,0 +1,17 @@
+package com.arturglier.mobile.android.soundscreen.common.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+public class IntentUtils {
+
+    public static boolean isActivityAvailable(Context context, Intent intent) {
+        final PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+}
